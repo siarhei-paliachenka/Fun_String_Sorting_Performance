01278b5527656644cb44931f0b2904e2070c4203
Siarhei Paliachenka <siarhei.paliachenka@gmail.com>
2024-02-18 13:29:20 +0100

    Using Span

--- StringSortingPerformance/Sorter/Item.cs
+++ StringSortingPerformance/Sorter/Item.cs
@@ -1,36 +1,30 @@
 ﻿namespace Sorter;
 
-public record Item: IComparable<Item>
+public record Item : IComparable<Item>
 {
     public Item(string source)
     {
         Source = source;
-        Text = GetText(source);
-        Index = GetIndex(source);
+        SpaceDividerIndex = GetSpaceDividerIndex(source);
     }
 
-    private int GetIndex(string source)
+    private int GetSpaceDividerIndex(string source)
     {
-        var indexOfStart = source.IndexOf(' ', StringComparison.InvariantCulture);
-        return int.Parse(source.Substring(0, indexOfStart - 1));
+        return source.IndexOf(' ', StringComparison.InvariantCulture);
     }
 
-    private string GetText(string source)
-    {
-        var indexOfStart = source.IndexOf(' ', StringComparison.InvariantCulture);
-        return source.Substring(indexOfStart);
-    }
+    public string Source { get; }
 
-    public string Source { get; init; }
-    public int Index { get; init; }
-    
-    public string Text { get; init; }
+    private int SpaceDividerIndex { get; }
+    private int Index => int.Parse(Source.AsSpan().Slice(0, SpaceDividerIndex - 1));
+
+    private ReadOnlySpan<char> Text => Source.AsSpan().Slice(SpaceDividerIndex + 1);
 
     public int CompareTo(Item? other)
     {
         if (ReferenceEquals(this, other)) return 0;
         if (ReferenceEquals(null, other)) return 1;
-        var textComparison = string.Compare(Text, other.Text, StringComparison.Ordinal);
+        var textComparison = Text.CompareTo(other.Text, StringComparison.Ordinal);
         if (textComparison != 0) return textComparison;
         return Index.CompareTo(other.Index);
     }
